cd088b7bcdd5366a1d73702fcbdc81c22b456ed9
Hugo Jimenez <hjimenez.upc@gmail.com>
2024-12-03 21:16:34 +0100

    Update SEO configuration and refresh sitemap with new last modified timestamps

--- next-seo.config.js
+++ next-seo.config.js
@@ -9,7 +9,7 @@ const SEO = {
     site_name: 'Hugo Jimenez - Machine Learning Engineer',
     images: [
       {
-        url: 'https://www.hugo-jimenez.com/link-preview-image.jpg', // Replace with your image URL
+        url: 'https://www.hugo-jimenez.com/link-preview-image.png', // Replace with your image URL
         width: 1200,
         height: 630,
         alt: 'Link preview image',
@@ -21,13 +21,7 @@ const SEO = {
     title: 'Hugo Jimenez - Machine Learning Engineer',
     description: 'Explore the portfolio of Hugo Jimenez, a Machine Learning Engineer specializing in AI, data science, and software development.',
     image: 'https://www.hugo-jimenez.com/link-preview-image.png', // Replace with your image URL
-    card: "summary_large_image",
-    image: {
-      url: 'https://www.hugo-jimenez.com/link-preview-image.png', // Replace with your image URL
-      width: 1200,
-      height: 630,
-      alt: 'Link preview image',
-    },
+    card: "summary_large_image"
   },
 };
 
