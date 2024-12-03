1cf27e8825d3b14bd35720db24ae4803c30bae7c
Hugo Jimenez <hjimenez.upc@gmail.com>
2024-12-03 20:36:29 +0100

    Add Iubenda scripts for cookie consent management in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import SEO from '../../next-seo.config';
 import '@/styles/globals.css';
 import ReactGA from 'react-ga';
 import { useEffect } from 'react';
+import Head from 'next/head';
 
 ReactGA.initialize('G-M0Q306LKMJ');
 
@@ -15,6 +16,15 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Head>
+        <script type="text/javascript">
+          {`var _iub = _iub || [];
+          _iub.csConfiguration = {"siteId":3858144,"cookiePolicyId":23602836,"lang":"en","storage":{"useSiteId":true}};`}
+        </script>
+        <script type="text/javascript" src="https://cs.iubenda.com/autoblocking/3858144.js"></script>
+        <script type="text/javascript" src="//cdn.iubenda.com/cs/gpp/stub.js"></script>
+        <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+      </Head>
       <DefaultSeo {...SEO} />
       <Navigation />
       <Component {...pageProps} />
