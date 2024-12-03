8bb6eab20327f1449eef1224f87da0e447682bb0
Hugo Jimenez <hjimenez.upc@gmail.com>
2024-12-03 20:45:12 +0100

    Remove Iubenda script integration from _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import SEO from '../../next-seo.config';
 import '@/styles/globals.css';
 import ReactGA from 'react-ga';
 import { useEffect } from 'react';
-import Script from 'next/script';
 
 ReactGA.initialize('G-M0Q306LKMJ');
 
@@ -16,30 +15,6 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-
-      <Script
-        id="iubenda-config"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            var _iub = _iub || [];
-            _iub.csConfiguration = {"siteId":3858144,"cookiePolicyId":23602836,"lang":"en","storage":{"useSiteId":true}};
-          `,
-        }}
-      />
-      <Script
-        src="https://cs.iubenda.com/autoblocking/3858144.js"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="//cdn.iubenda.com/cs/gpp/stub.js"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="//cdn.iubenda.com/cs/iubenda_cs.js"
-        strategy="afterInteractive"
-        async
-      />
       <DefaultSeo {...SEO} />
       <Navigation />
       <Component {...pageProps} />
