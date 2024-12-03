54910359deb89f8b2e17d00ad493f2cea00f858e
hugo <hugo@carina.chat>
2024-12-03 11:05:17 +0100

    adds seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,21 +1,13 @@
-
 const SEO = {
-  title: 'Your Name - Machine Learning Engineer',
-  description: 'Personal portfolio website showcasing my projects and blog.',
+  title: 'Hugo Jimenez - Machine Learning Engineer',
+  description: 'Personal portfolio website showcasing my projects, skills, and blog in the field of machine learning and AI.',
   openGraph: {
     type: 'website',
-    url: 'https://www.yourdomain.com/',
-    title: 'Your Name - Machine Learning Engineer',
-    description: 'Personal portfolio website showcasing my projects and blog.',
-    images: [
-      {
-        url: 'https://www.yourdomain.com/og-image.jpg',
-        width: 800,
-        height: 600,
-        alt: 'Og Image Alt',
-      },
-    ],
-  },
+    url: 'https://www.hugo-jimenez.com/',
+    title: 'Hugo Jimenez - Machine Learning Engineer',
+    description: 'Explore the portfolio of Hugo Jimenez, a Machine Learning Engineer specializing in AI, data science, and software development.',
+
+  }
 };
 
 export default SEO;
