636d712c6a676115fbb0cbdc903f83396cf07ae4
hugo <hugo@carina.chat>
2024-12-03 13:43:52 +0100

    Add Google Analytics integration with react-ga for tracking page views

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,16 @@ import Navigation from '@/components/Navigation';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../../next-seo.config';
 import '@/styles/globals.css';
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
+
+ReactGA.initialize('G-M0Q306LKMJ');
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <>
       <DefaultSeo {...SEO} />
