e1e9d13f9bcb8716561ea0a1301f395410de070a
hugo <hugo@carina.chat>
2024-12-03 11:05:30 +0100

    adds seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app';
 import Navigation from '@/components/Navigation';
+import { DefaultSeo } from 'next-seo';
+import SEO from '../../next-seo.config';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo {...SEO} />
       <Navigation />
       <Component {...pageProps} />
     </>
